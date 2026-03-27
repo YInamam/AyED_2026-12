52c76fa03cd612519c5d46c5cc04edf29881d092
YInamam <aspimamaniyvonne@gmail.com>
2026-03-27 16:45:30 -0300

    Add files via upload

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string juego;
+            string consol;
+
+            Console.WriteLine("¿Hola usuario?");
+            Console.Write("¿Como te llamas?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu juego favorito?: ");
+            juego = Console.ReadLine();
+            Console.Write("¿En que consola jugas?: ");
+            consol = Console.ReadLine();
+
+            Console.WriteLine("hola " + nombre + " Tu juego favorito es " + juego + " y se juega en " + consol);
+        }
+    }
+}
+
