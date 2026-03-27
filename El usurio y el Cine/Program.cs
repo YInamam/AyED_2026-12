aaff8756e127a4ff0bcc8fb456b44202bc55986b
YInamam <aspimamaniyvonne@gmail.com>
2026-03-27 16:31:06 -0300

    Add files via upload

---
+++ El usurio y el Cine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_usurio_y_el_Cine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string pelicula;
+
+            Console.WriteLine("¿Hola usuario?");
+            Console.Write("¿Como te llamas?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu pelicula favorita?: ");
+            pelicula = Console.ReadLine();
+
+            Console.WriteLine("hola " + nombre + " Tu pelicula preferida " + pelicula + " es una porqueria " );
+        }
+    }
+}
