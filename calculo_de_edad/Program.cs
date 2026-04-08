c49df4bc1b67222439c805a5b151c7f8b93ca789
YInamam <aspimamaniyvonne@gmail.com>
2026-04-08 14:29:38 -0300

    Add files via upload

---
+++ calculo_de_edad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_de_edad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("¿en que año nasiste? ");
+            int año = Convert.ToInt32(Console.ReadLine());
+            int edad = 2026 - año;
+            Console.WriteLine("Si naciste en el año " + año + " tu edad al comenzar el año es de " + edad + " años."); 
+        }
+    }
+}
