2a96c859718e295fafdbdd359949f8a2323f30ee
YInamam <aspimamaniyvonne@gmail.com>
2026-04-08 14:50:10 -0300

    Add files via upload

---
+++ operadores_aritmeticos1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operadores_aritmeticos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("escribe un valor ");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("escribe valor ");
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+            int suma = valor1 + valor2;
+            int resta = valor1 - valor2;
+            int multiplicacion = valor1 * valor2;
+            int divicion = valor1 / valor2;
+            Console.WriteLine("La suma de los dos valores es " + suma + " la resta es " + resta + " la multiplicacion es " + multiplicacion + " y la divicion es " + divicion);
+        }
+    }
+}
