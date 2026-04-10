18abfb2a91e03a776269d9886310ccb40e0a6dec
YInamam <aspimamaniyvonne@gmail.com>
2026-04-10 16:16:25 -0300

    Add files via upload

---
+++ ejersitacion IF - ELSE/Calculadora de Saltos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_Saltos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese la distancia al destino ");
+            int distancia = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ingrese el combustible actual ");
+            int conbustible = Convert.ToInt32(Console.ReadLine());
+            int luz = distancia * 12;
+            int falta = luz - conbustible;
+            if (luz <= conbustible)
+            {
+                Console.WriteLine("Cálculos precisos. Saltando al hiperespacio");
+            }
+            else
+            {
+                Console.WriteLine("Peligro: Combustible insuficiente. Faltan "+ falta +" litros");
+            }
+        }
+    }
+}
