1f3fce301555484e3489257368f8a0292aec1ecc
YInamam <aspimamaniyvonne@gmail.com>
2026-04-15 15:23:09 -0300

    Add files via upload

---
+++ ejersitacion IF - ELSE/Cuestionario de Temperamento/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuestionario_de_Temperamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("encontras un cristal Kyberperdido ");
+            Console.WriteLine("¿que haces?");
+            Console.WriteLine("Opción 1: El camino del Jedi sigue ");
+            Console.WriteLine("Opción 2: Un cazarrecompensas eres ");
+            Console.WriteLine("Opción 3: El Lado Oscuro se apodera de ti ");
+            Console.WriteLine("ingrese un numero");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            if (opcion == 1)
+            {
+                Console.WriteLine("El camino del Jedi");
+            }
+            else
+            {
+                if (opcion == 2)
+                {
+                    Console.WriteLine("Un caza recompensas eres ");
+                }
+                else
+                {
+                    if (opcion == 3)
+                    {
+                        Console.WriteLine("el lado oscuro se apodera de ti ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error en la Fuerza: opción inválida");
+                    }
+
+                }
+            }
+        }
+    }
+}
+
