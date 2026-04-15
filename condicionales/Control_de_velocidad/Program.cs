f09cf9f8afd4881ef5c9cde26bdce9264ca41ace
YInamam <aspimamaniyvonne@gmail.com>
2026-04-15 16:44:50 -0300

    Add files via upload

---
+++ condicionales/Control_de_velocidad/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_de_velocidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese la velosidad de un vehiculo: ");
+            int Veloc = Convert.ToInt32(Console.ReadLine());
+            if (Veloc >= 120) 
+            {
+                Console.WriteLine("Multa en camino: Exceso de velocidad");
+            }
+            else
+            {
+                Console.WriteLine("Conducción segura");
+            }
+        }
+    }
+}
