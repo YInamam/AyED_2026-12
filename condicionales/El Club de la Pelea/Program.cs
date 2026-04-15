0f698cc59bf1d7d7d00f0d2caaf7f26e18e36d74
YInamam <aspimamaniyvonne@gmail.com>
2026-04-15 16:46:21 -0300

    Add files via upload

---
+++ condicionales/El Club de la Pelea/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_Club_de_la_Pelea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("ingrese su nombre ");
+            nombre = Console.ReadLine();
+            Console.Write("ingrese su edad ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+         
+            if ( edad >= 18 )
+            {
+                Console.WriteLine("Bienvenido al club, " + nombre);
+
+            }
+            else
+            {
+                Console.WriteLine("Lo siento, eres muy polluelo para esto");
+            }
+        }
+    }
+}
+
