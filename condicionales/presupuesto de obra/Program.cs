c75be0f91f1f9401d87d967df665742875fcfd73
YInamam <aspimamaniyvonne@gmail.com>
2026-04-24 16:25:46 -0300

    Add files via upload

---
+++ condicionales/presupuesto de obra/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace presupuesto_de_obra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese el presupuesto disponible ");
+            int presupuesto = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ingrese el costo de lo materiale ");
+            int costo = Convert.ToInt32(Console.ReadLine());
+            if (costo > presupuesto)
+            {
+                int calculo = costo - presupuesto;
+                Console.WriteLine("hay que pedir prestado " + calculo);
+            }
+        }
+    }
+}
