85eecdd939b53368c072e81d4f49b3dbbdd27b83
YInamam <aspimamaniyvonne@gmail.com>
2026-04-24 16:24:21 -0300

    Add files via upload

---
+++ condicionales/multiplos de 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplos_de_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("escribe un numero ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int result = numero % 5;
+            if (result == 0)
+            {
+                Console.Write("el numero ingresado es multiplo de 5 ");
+            }
+            else
+            {
+                Console.WriteLine("el numero ingresado no es multiplo de 5");
+            }
+        }
+    }
+}
