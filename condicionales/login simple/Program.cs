4fddf18b81a709451f02e21a4064329d295d9d34
YInamam <aspimamaniyvonne@gmail.com>
2026-04-24 16:22:36 -0300

    Add files via upload

---
+++ condicionales/login simple/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace login_simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese un nombre de usuario ");
+            string usuario = Console.ReadLine();
+            Console.Write("ingrese contraseña ");
+            string contraseña = Console.ReadLine();
+            if (usuario == "admin")
+            {
+                if (contraseña == "1234")
+                {
+                    Console.WriteLine("acceso consedido");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error de credenciales");
+            }
+            
+        }
+    }
+}
