af52558b7989352e69a00a260addc81c1b3087f2
YInamam <aspimamaniyvonne@gmail.com>
2026-04-24 16:23:41 -0300

    Add files via upload

---
+++ condicionales/clima de montaña/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clima_de_montaña
+{
+    class Program
+    {
+        public static object ToInt32 { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese temperatura actual ");
+            int temp = Convert.ToInt32(Console.ReadLine());
+            if (temp < 0)
+            {
+                Console.WriteLine("Peligro de congelamiento");
+            }
+            else if (temp > 0 && temp < 15)
+            {
+                Console.WriteLine("Mucho frío");
+            }
+            else
+            {
+                Console.WriteLine("Clima agradable");
+            }
+            
+        }
+    }
+}
