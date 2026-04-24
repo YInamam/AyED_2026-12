66d33e6cf27fc030761e4249c273b4673b9e8789
YInamam <aspimamaniyvonne@gmail.com>
2026-04-24 16:21:36 -0300

    Add files via upload

---
+++ condicionales/calculadora de descuentos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora_de_descuentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese el monto de la compra ");
+            int monto = Convert.ToInt32(Console.ReadLine());
+            if (monto > 5000)
+            {
+                int porcent = 15;
+                int descuent = monto * porcent / 100;
+                Console.WriteLine("tiene que pagar " + descuent );
+            }
+        }
+    }
+}
