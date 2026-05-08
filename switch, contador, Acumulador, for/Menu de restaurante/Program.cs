e308d1d8573d58eb4fe7d5616c7b6b1548398d00
YInamam <aspimamaniyvonne@gmail.com>
2026-05-08 16:38:56 -0300

    Add files via upload

---
+++ switch, contador, Acumulador, for/Menu de restaurante/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_de_restaurante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            Console.WriteLine("(S)ándwich");
+            Console.WriteLine("(P)izza");
+            Console.WriteLine("(H)amburguesa.");
+            Console.WriteLine("ingrese lo seleccionado ");
+            
+            string selec = Console.ReadLine();
+
+            switch (selec)
+            {
+                case "S":
+                    Console.WriteLine("(S)andwich esta 3000 ");
+                    break;
+                case "P":
+                    Console.WriteLine("(P)izz esta 2500 ");
+                    break;
+                case "H":
+                    Console.WriteLine("(H)amburguesa esta 1000 ");
+                    break;
+                default:
+                    Console.WriteLine("Producto no encontrado");
+                    break;
+
+            }
+        }
+    }
+}
