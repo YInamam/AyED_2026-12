845e2e2cc3905c8a431c2a00d8b4422aeaf28f83
YInamam <aspimamaniyvonne@gmail.com>
2026-05-08 16:41:31 -0300

    Add files via upload

---
+++ switch, contador, Acumulador, for/selector de idiomas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selector_de_idiomas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elija un idioma  ");
+            Console.WriteLine("1:Inglés ");
+            Console.WriteLine("2:Francés ");
+            Console.WriteLine("3:Alemán ");
+            Console.WriteLine("ingrese un el idioma elegido ");
+
+            int idiom = int .Parse(Console.ReadLine());
+
+            switch (idiom)
+            {
+                case 1 :
+                    Console.WriteLine("Hello ");
+                    break;
+                case 2 :
+                    Console.WriteLine("Salud ");
+                    break;
+                case 3 :
+                    Console.WriteLine("Hallo ");
+                    break;
+                
+            }
+        }
+    }
+}
