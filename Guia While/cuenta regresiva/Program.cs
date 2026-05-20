8e3d9aa3ac474fed81138722d3dbad6a772c29cd
YInamam <aspimamaniyvonne@gmail.com>
2026-05-20 16:41:28 -0300

    Add files via upload

---
+++ Guia While/cuenta regresiva/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cuenta_regresiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese un numero entero positivo ");
+            int numer = int.Parse(Console.ReadLine());
+            Console.WriteLine("cuenta regresiva ");
+            while (numer >= 0)
+            {
+                Console.WriteLine(numer );
+                numer--;
+            }
+        }
+    }
+}
