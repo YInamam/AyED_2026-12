5bf39c3bca11ca48d832e0a8825a267a3a40a21a
YInamam <aspimamaniyvonne@gmail.com>
2026-05-27 16:09:06 -0300

    Add files via upload

---
+++ Guia While/Sumador hasta el Cero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumador_hasta_el_Cero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int numero = 0;
+
+            do
+            {
+                Console.Write("Ingrese numero entero para sumarlo: ");
+                numero = int.Parse(Console.ReadLine());
+                suma = suma + numero;
+            } while (numero > 0 || numero < 0);
+
+            Console.WriteLine("total acumulado es " + suma);
+        }
+    }
+}
