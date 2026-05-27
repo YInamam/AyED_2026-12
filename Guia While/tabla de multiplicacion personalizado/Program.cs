ef323b95eb2351eaaa22467e8a09f8c05ad237ff
YInamam <aspimamaniyvonne@gmail.com>
2026-05-27 16:13:54 -0300

    Add files via upload

---
+++ Guia While/tabla de multiplicacion personalizado/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabla_de_multiplicacion_personalizado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
