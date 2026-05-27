c3dccfac44342d8a30fb8d71c0808e6d521003ce
YInamam <aspimamaniyvonne@gmail.com>
2026-05-27 16:40:44 -0300

    Add files via upload

---
+++ Guia While/Promedio de notas simplee/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio_de_notas_simplee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadr = 2;
+            int suma = 0;
+
+            while (contadr <= 5)
+            {
+                Console.Write("Ingrese la nota " + contadr + ": ");
+                int  nota = int.Parse(Console.ReadLine());
+                suma = suma + nota;
+                contadr++;
+            }
+
+            int promedio = suma / 5;
+
+            Console.WriteLine("El promedio es: " + promedio);
+
+
+        }
+    }
+}
