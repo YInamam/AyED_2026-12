8f4809110eeb73dc23d5c67196a417528ed1523f
YInamam <aspimamaniyvonne@gmail.com>
2026-05-29 14:47:35 -0300

    Add files via upload

---
+++ Guia While/multiplicacion por sumas susesivas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicacion_por_sumas_susesivas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.Write("Ingrese el número (A) ");
+            int  A = int.Parse(Console.ReadLine());
+
+          Console.Write("Ingrese el número (B) ");
+            int  B = int.Parse(Console.ReadLine());
+            int resultado = 0;
+            int contador = 0;
+
+
+            while (contador < B)
+            {
+                resultado = resultado + A;
+                contador++;
+            }
+
+            Console.WriteLine("Resultado: " + resultado);
+        }
+
+    }
+}
