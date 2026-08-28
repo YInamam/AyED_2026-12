a7f179edf560ebebd545943330b886e8755b397a
YInamam <aspimamaniyvonne@gmail.com>
2026-08-28 16:37:35 -0300

    Add files via upload

---
+++ funciones/ejersicio 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejersicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese un numero positivo: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = SumarHasta(num);
+            Console.WriteLine("La suma es: " + result);
+        }
+
+        static int SumarHasta(int num)
+        {
+            int mult = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                mult = mult * i;
+
+            }
+            return mult;
+
+        }
+
+
+    }
+
+}
