59eb5459b86d3c083c580b10d613996cf6b45a71
YInamam <aspimamaniyvonne@gmail.com>
2026-08-28 14:32:49 -0300

    Add files via upload

---
+++ funciones/ejersicio 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejersicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero positivo: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            ContarRegresivo(num);
+        }
+
+        static void ContarRegresivo(int numero)
+        {
+            while (numero >= 1)
+            {
+                numero--;
+
+                Console.WriteLine(numero);
+            }
+        }
+
+
+    }
+}
