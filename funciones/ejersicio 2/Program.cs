574a7fb48e6a2f3cc792316033a70f58fa0140b1
YInamam <aspimamaniyvonne@gmail.com>
2026-08-28 15:29:25 -0300

    Add files via upload

---
+++ funciones/ejersicio 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejersicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+            {
+                Console.Write("ingrese un numero positivo: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                ContarHasta(num);
+            }
+
+            static void ContarHasta(int num)
+        {
+                int contador = 1;
+
+                while (contador <= num)
+                {
+                    Console.WriteLine(contador);
+                    contador++;
+                }
+            }
+
+        }
+
+    }
+    
